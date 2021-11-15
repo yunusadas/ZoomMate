a30fa1081662b2e719cfb4595d7b2b670ea5d3f0
Yunus Emre Adaş <55918286+yunusadas@users.noreply.github.com>
2021-11-16 02:08:13 +0300

    Add files via upload

---
+++ zoommate.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from datetime import datetime
+
+start_time = datetime.now()
+
+
+browser = webdriver.Chrome(executable_path="chromedriver")
+
+browser.get('http://lms.fsm.edu.tr/login/ogrenci')
+
+email= browser.find_element_by_id('username') 
+email.send_keys('yunusemre.adas')
+
+password= browser.find_element_by_id('password') 
+password.send_keys('Yu11288329554')
+
+password.submit()
+
+#browser.find_element_by_class_name('event').click()
+#browser.find_element_by_css_selector(".event:firstchild[data-region='event-item']").click()
+#https://fsm-edu-tr.zoom.us/j/91380464854?pwd=QnppbmlSbERYb3MwMjFNOVRVNDU5dz09 - sayısal sistemler
+
